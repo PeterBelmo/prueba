78ca5cda54f5d795d603134eab51b5115fb76de0
Miquel Marin <miquel111135@gmail.com>
2017-05-19 09:20:24 +0200

    primer commit

---
+++ Documents/NetBeansProjects/pedrojuanbelmonte/proyectoGitHub/src/HolaMundo/holaMundo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package HolaMundo;
+
+/*
+ *
+ * holaMundo.java
+ * Creada el 19-may-2017,8:32:42
+ *
+ * Desarrollada por miquelpedro en la empresa Abastos.
+ /**
+ *
+ * @author $MiquelyPedro
+ * @version 1.0
+ * @company $abastos micorreo@gmail.com
+ * @Fecha 19-may-2017
+ */
+public class holaMundo {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello Earth!!!");
+    }
+}
